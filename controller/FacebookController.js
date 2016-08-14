6697baf717aaa538dc9e222b1c7edb347b136268
Pittayarat <pittayarat.sae@gmail.com>
2016-08-14 22:11:04 +0700

    move hardCode UserID to Controller

--- controller/FacebookController.js
+++ controller/FacebookController.js
@@ -5,20 +5,31 @@ import * as FacebookService from '../service/FacebookService'
 
 let facebookRouter = express.Router()
 
+var userIDs = ["CIEatKMITL","kmitl001","sorkmitl","1749829098634111","kmitl.engineer.inter","CE.KMITL"];
+var limit="";
+var since="2016-08-01";
+var until="";
+
+
 facebookRouter.route('/').get((req, res) => {
   res.send('<h1>Hello Facebook</h1>')
 })
 
 facebookRouter.route('/getDetail').get((req, res) => {
-   FacebookService.getDetail()
-   .then(result =>{
-      res.send(result);
-   })
+
+   var detail
+   //for(var user of userIDs){
+      FacebookService.getDetail(user)
+      .then(result =>{
+          detail.push(result)
+      })
+   //}
+   
 })
 
 facebookRouter.route('/getFeed').get((req, res) => {
   
-  FacebookService.getFeed()
+  FacebookService.getFeed("CIEatKMITL",since,until)
    .then(result =>{
       var body=""
       for(var data of result.data){
