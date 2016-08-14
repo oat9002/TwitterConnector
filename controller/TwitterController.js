3ae3c0ccad6415e3b20cfa9de777323b45bd6f17
oato.o <oat9002@gmail.com>
2016-08-14 14:54:34 +0700

    Separated route

--- controller/TwitterController.js
+++ controller/TwitterController.js
@@ -1,6 +1,25 @@
 "use strict";
+import express from 'express'
 import * as TwitterService from '../service/TwitterService'
 
-export function tweet(data) {
-  TwitterService.tweet(data.status)
-}
+let twitterRouter = express.Router()
+
+twitterRouter.route('/').get((req, res) => {
+  res.send('<h1>Hello Twitter</h1>')
+})
+
+twitterRouter.route('/index').get((req, res) => {
+  res.send('<h1>This is index page</h1>')
+})
+
+twitterRouter.route('/tweet').post((req, res) => {
+  res.send(TwitterService.tweet(req.body.status))
+})
+
+twitterRouter.route('/search').post(function*(req, res) {
+  let result = yield TwitterService.search(req.body.q, req.body.count)
+  console.log('result', result);
+  // res.send(result)
+})
+
+export default twitterRouter
