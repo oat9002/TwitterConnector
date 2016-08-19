e194840a664ef54d2ffb4c5bc20bf68bc44aa56b
Pittayarat <pittayarat.sae@gmail.com>
2016-08-19 16:02:29 +0700

    add mongojs db setup (in comment)

--- db.js
+++ db.js
@@ -9,3 +9,13 @@ export const sequelize = new Sequelize('SocialRest', 'rest', 'restapi', {
     idle: 10000
   },
 })
+
+// import mongojs from 'mongojs'
+
+// let databaseUrl = 'devahoy_mongojs'
+// let collections = ['Facebook', 'Twitter']
+
+// let connect = mongojs(databaseUrl, collections)
+
+// export const facebook = connect.collection('Facebook')
+// export const twitter = connect.collection('Twitter')
